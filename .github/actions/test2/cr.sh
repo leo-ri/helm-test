aca287cc5ab305c9c046bc771c9182b87b6c6ab5
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2021-10-27 14:24:22 +0300

    test

--- .github/actions/test2/cr.sh
+++ .github/actions/test2/cr.sh
@@ -3,7 +3,11 @@
 set -eou pipefail
 
 # simple changelog: get all commits beetween 2 tags and put them into file
-echo "${INPUT_TEST}"
-IFS=" " read -r -a dependencies<<<"${INPUT_TEST}"
+test=$1
+test2=$2
+IFS=" " read -r -a dependencies<<<"${test}"
+mapfile -t t2< <(echo "$test2")
 echo "dependencies: " "${dependencies[*]}"
 echo "dependencies1:" "${dependencies[1]}"
+echo "t2: " "${t2[*]}"
+echo "t2: " "${t2[1]}"
